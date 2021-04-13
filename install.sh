700e94b10fca28a90b7db16b428164a9127bda13
Duy Nguyen <duy210697@gmail.com>
2021-04-13 17:24:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 # copies all config as symlinks
+brew install neovim tmux zsh
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -s * ~/
